170e0960417a24e08043946794d81f903a57d62b
Am1d1v <dimadiman1adima@gmail.com>
2024-04-09 13:37:05 +0300

    Pages added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
 function Footer() {
+
+
+
   return (
-    <div>Footer</div>
+    <footer className="page-footer deep-purple lighten-3">
+          <div className="footer-copyright">
+            <div className="container">
+            © {new Date().getFullYear()} Copyright Text
+            <a className="grey-text text-lighten-4 right" href="https://github.com/Am1d1v">Repo</a>
+            </div>
+          </div>
+        </footer>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
 function Header() {
+
   return (
-    <div>Header</div>
+    <nav>
+        <div className="nav-wrapper deep-purple lighten-3">
+          <a href="https://github.com/Am1d1v" className="brand-logo">React Meal SPA</a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li><a href="https://github.com/Am1d1v">Repo</a></li>
+          </ul>
+        </div>
+    </nav>
   )
 }
 
